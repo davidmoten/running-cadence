ce9ec738cc196364b59e64f037f665987730fc84
Dave Moten <davidmoten@gmail.com>
2013-05-27 17:34:58 +1000

    update android and pom xml

--- src/main/java/org/moten/david/HelloAndroidActivity.java
+++ src/main/java/org/moten/david/HelloAndroidActivity.java
@@ -1,30 +1,79 @@
 package org.moten.david;
 
+import java.text.DecimalFormat;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class HelloAndroidActivity extends Activity {
+public class HelloAndroidActivity extends Activity implements
+		SensorEventListener {
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(org.moten.david.R.menu.main, menu);
-	return true;
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
+
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(org.moten.david.R.menu.main, menu);
+		return true;
+	}
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// ignore
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+
+		DecimalFormat df = new DecimalFormat("0.000");
+		StringBuilder s = new StringBuilder();
+		s.append(df.format(x)).append(",").append(df.format(y)).append(",")
+				.append(df.format(z));
+
+		TextView view = (TextView) findViewById(R.id.edit_message);
+		view.setText(s.toString());
+	}
 
 }
-
